5dc944daa86bbbaf96718aca7929bc4329029a9f
MstMoonshine <38682200@qq.com>
2021-06-11 19:09:55 +0800

    scripts update

--- gdb.sh
+++ gdb.sh
@@ -1,6 +1,6 @@
-qemu-system-riscv64 -M virt -m 256M -nographic -S -s\
+qemu-system-riscv64 -M virt -m 256M -nographic -s -S\
         -kernel build/platform/qemu/virt/firmware/fw_jump.elf \
-        -device loader,file=Image,addr=0x80200000 \
+        -device loader,file=/home/prongs/src/linux/arch/riscv/boot/Image,addr=0x80200000 \
         -drive file=/home/prongs/src/busybox/root.bin,format=raw,id=hd0 \
         -device virtio-blk-device,drive=hd0 \
         -append "root=/dev/vda rw console=ttyS0"
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 qemu-system-riscv64 -M virt -m 256M -nographic \
         -kernel build/platform/qemu/virt/firmware/fw_jump.elf \
-        -device loader,file=Image,addr=0x80200000 \
-        -drive file=/media/prongs/S/src/busybox/root.bin,format=raw,id=hd0 \
+        -device loader,file=/home/prongs/src/linux/arch/riscv/boot/Image,addr=0x80200000 \
+        -drive file=/home/prongs/src/busybox/root.bin,format=raw,id=hd0 \
         -device virtio-blk-device,drive=hd0 \
         -append "root=/dev/vda rw console=ttyS0"

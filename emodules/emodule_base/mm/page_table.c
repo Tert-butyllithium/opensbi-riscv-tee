9806e5353063db308f39d6ce27c8d12e34de4463
Tert-butyllithium <lihaonan1105@gmail.com>
2021-07-03 08:32:13 +0000

    fix bug: error for many pages alloc

--- emodules/emodule_base/mm/page_table.c
+++ emodules/emodule_base/mm/page_table.c
@@ -9,7 +9,8 @@
 #define printd printf
 #endif
 
-page_directory page_directory_pool[64] __attribute__((section(".page_table")));
+static page_directory page_directory_pool[PAGE_DIR_POOL] __attribute__((section(".page_table")));
+static trie address_trie __attribute__((section(".page_table_trie")));
 
 /**
  * insert a va-pa pair to page table, maintained via a trie
@@ -31,8 +32,8 @@ static uintptr_t trie_get_or_insert(trie *t, const uintptr_t va,
      * Note: l[0] = L1, l[2] = l[0]
      */
 
-	uintptr_t l[] = { (va & MUSK_L2) >> 30, (va & MUSK_L1) >> 21,
-			  (va & MUSK_L0) >> 12 };
+	uintptr_t l[] = { (va & MASK_L2) >> 30, (va & MASK_L1) >> 21,
+			  (va & MASK_L0) >> 12 };
 	pte *tmp_pte;
 
 	// for a three level page table, only two PPNs need to point to next level
@@ -52,9 +53,9 @@ static uintptr_t trie_get_or_insert(trie *t, const uintptr_t va,
 	tmp_pte	     = &page_directory_pool[p][l[len - 1]];
 	tmp_pte->ppn = pa >> 12;
 	if (len == 2) {
-		tmp_pte->ppn = (tmp_pte->ppn | MUSK_OFFSET) ^ MUSK_OFFSET;
+		tmp_pte->ppn = (tmp_pte->ppn | MASK_OFFSET) ^ MASK_OFFSET;
 	}
-	tmp_pte->pte_v = tmp_pte->pte_d = 1;
+	tmp_pte->pte_v = tmp_pte->pte_d = tmp_pte->pte_a = 1;
 	tmp_pte->pte_r = tmp_pte->pte_w = tmp_pte->pte_x = 1;
 	return *((uintptr_t *)tmp_pte);
 }
@@ -62,7 +63,6 @@ static uintptr_t trie_get_or_insert(trie *t, const uintptr_t va,
 static uintptr_t page_directory_insert(uintptr_t va, uintptr_t pa, int levels,
 				       int attr)
 {
-	static trie address_trie;
 	uintptr_t p = (uintptr_t)trie_get_or_insert(&address_trie, va, pa,
 						    levels, attr);
 	return p;
@@ -75,10 +75,10 @@ uintptr_t get_page_table_root()
 
 uintptr_t get_pa(uintptr_t va)
 {
-	uintptr_t l[] = { (va & MUSK_L2) >> 30, (va & MUSK_L1) >> 21,
-			  (va & MUSK_L0) >> 12 };
+	uintptr_t l[] = { (va & MASK_L2) >> 30, (va & MASK_L1) >> 21,
+			  (va & MASK_L0) >> 12 };
 	pte entry;
-	pte *root = page_directory_pool[0];
+	pte *root = (void*) get_page_table_root();
 	pte tmp_entry;
 	uintptr_t tmp;
 	int i = 0;
@@ -108,13 +108,13 @@ void map_page(pte *root, uintptr_t va, uintptr_t pa, size_t n_pages,
 	      uintptr_t attr)
 {
 	pte *pt;
-
-	while (n_pages >= 512) {
-		page_directory_insert(va, pa, 2, attr);
-		va += EPAGE_SIZE * 512;
-		pa += EPAGE_SIZE * 512;
-		n_pages -= 512;
-	}
+	printd("[doing_map_page]map_page(nullptr,0x%lx,0x%lx,%d,0);\n",va,pa,n_pages);
+	// while (n_pages >= 512) {
+	// 	page_directory_insert(va, pa, 2, attr);
+	// 	va += EPAGE_SIZE * 512;
+	// 	pa += EPAGE_SIZE * 512;
+	// 	n_pages -= 512;
+	// }
 
 	while (n_pages >= 1) {
 		page_directory_insert(va, pa, 3, attr);
@@ -126,12 +126,13 @@ void map_page(pte *root, uintptr_t va, uintptr_t pa, size_t n_pages,
 
 uintptr_t ioremap(pte *root, uintptr_t pa, size_t size)
 {
-	static uintptr_t drv_start_va = 0;
+	static uintptr_t drv_addr_alloc = 0;
+	printd("current root address: %p",get_page_table_root());
 	size_t n_pages		      = PAGE_UP(size) >> EPAGE_SHIFT;
-	map_page(root, drv_start_va + EDRV_DRV_START, pa, n_pages,
+	map_page(root,  EDRV_DRV_START + drv_addr_alloc, pa, n_pages,
 		 PTE_V | PTE_W | PTE_R | PTE_D | PTE_X);
-	uintptr_t cur_addr = drv_start_va + EDRV_DRV_START;
-	drv_start_va += n_pages << EPAGE_SHIFT;
+	uintptr_t cur_addr =  EDRV_DRV_START;
+	drv_addr_alloc += n_pages << 12;
 	return cur_addr;
 }
 

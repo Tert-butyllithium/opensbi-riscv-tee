4c5c5a701f58f800660d486666036ec45966beb4
MstMoonshine <38682200@qq.com>
2021-07-05 11:25:20 +0800

    va_pa_offset

--- emodules/emodule_base/mm/page_table.c
+++ emodules/emodule_base/mm/page_table.c
@@ -43,7 +43,7 @@ static uintptr_t trie_get_or_insert(trie *t, const uintptr_t va,
 			printd("page cnt:%d\n", t->cnt);
 			tmp_pte = &page_directory_pool[p][l[i]];
 			tmp_pte->ppn =
-				((uintptr_t)&page_directory_pool[t->cnt][0])>>12;
+				(((uintptr_t)&page_directory_pool[t->cnt][0]) - va_pa_offset())>>12;
 			tmp_pte->pte_v = tmp_pte->pte_d = 1;
 		}
 		p = t->next[p][l[i]];
@@ -91,7 +91,7 @@ uintptr_t get_pa(uintptr_t va)
 		if ((tmp_entry.pte_r | tmp_entry.pte_w | tmp_entry.pte_x)) {
 			break;
 		}
-		tmp  = tmp_entry.ppn << 12;
+		tmp  = (tmp_entry.ppn + va_pa_offset()) << 12;
 		root = (pte *)tmp;
 		i++;
 	}

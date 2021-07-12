594d24217a68f1a7e0c36af1686acdbd82dfb93f
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-07-12 15:43:39 +0800

    qemu run script

--- gdb.sh
+++ gdb.sh
@@ -1,9 +1,9 @@
-qemu-system-riscv64 -M virt -m 256M -nographic -s -S\
+qemu-system-riscv64 -M virt -m 1024M -nographic -s -S\
         -kernel build/platform/qemu/virt/firmware/fw_jump.elf \
         -device loader,file=/home/prongs/src/linux/arch/riscv/boot/Image,addr=0x82000000 \
         -drive file=/home/prongs/src/busybox/root.bin,format=raw,id=hd0 \
         -device virtio-blk-device,drive=hd0 \
-        -append "root=/dev/vda rw console=ttyS0"
+        -append "root=/dev/vda rw console=ttyS0 movablecore=0x10000000"
 
 
 # add-symbol-file ~/src/linux/vmlinux
--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
-qemu-system-riscv64 -M virt -m 256M -nographic \
+qemu-system-riscv64 -M virt -m 1024M -nographic\
         -kernel build/platform/qemu/virt/firmware/fw_jump.elf \
         -device loader,file=/home/prongs/src/linux/arch/riscv/boot/Image,addr=0x82000000 \
         -drive file=/home/prongs/src/busybox/root.bin,format=raw,id=hd0 \
         -device virtio-blk-device,drive=hd0 \
-        -append "root=/dev/vda rw console=ttyS0"
+        -append "root=/dev/vda rw console=ttyS0 movablecore=0x20000000"
+
 
 #mount -t proc p /proc
 #mount -t sysfs sysfs /sys

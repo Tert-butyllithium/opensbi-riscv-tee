71411a7664ee3c3b1928173d8a9877a985f48dbe
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-07-15 19:53:06 +0800

    [cherry-pick] enclave exception exit correct eid

--- emodules/emodule_base/drv_handler.c
+++ emodules/emodule_base/drv_handler.c
@@ -27,10 +27,12 @@ void handle_interrupt(uintptr_t* regs, uintptr_t scause, uintptr_t sepc, uintptr
     }
 }
 
-void handle_exception(uintptr_t* regs, uintptr_t scause, uintptr_t sepc, uintptr_t stval) {
-    printd("handle exception %d 0x%llx  0x%llx!\n", scause, sepc, stval);
-    // SBI_CALL(EBI_EXIT, 0, 0, 0);
-    SBI_CALL5(SBI_EXT_EBI, enclave_id, 0, 0, EBI_EXIT);
+void handle_exception(uintptr_t *regs, uintptr_t scause, uintptr_t sepc,
+		      uintptr_t stval)
+{
+	printd("handle exception %d 0x%llx  0x%llx!\n", scause, sepc, stval);
+	// SBI_CALL(EBI_EXIT, 0, 0, 0);
+	SBI_CALL5(SBI_EXT_EBI, enclave_id, 0, 0, EBI_EXIT);
 }
 
 void handle_syscall(uintptr_t* regs, uintptr_t scause, uintptr_t sepc, uintptr_t stval) {
